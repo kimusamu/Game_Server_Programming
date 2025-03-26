cec759aa8e18f9ef1354e064a9a1dda68a13a427
Kim Sang Yun <grinder0617@gmail.com>
2025-03-27 03:53:50 +0900

    2025.03.27
    make chess server to overlapping

--- ChessClub/server.cpp
+++ ChessClub/server.cpp
@@ -12,6 +12,16 @@ constexpr int rows = 8, cols = 8;
 
 int player_x = 0, player_y = 0;
 
+char recv_buffer[1024];
+WSABUF recv_wsabuf[1];
+WSAOVERLAPPED recv_over;
+
+char send_buffer[1024];
+WSABUF send_wsabuf[1];
+WSAOVERLAPPED send_over;
+
+SOCKET c_socket;
+
 void print_error_message(int s_err)
 {
 	WCHAR* lpMsgBuf;
@@ -62,6 +72,9 @@ void update_position(const std::string& command)
 	}
 }
 
+void CALLBACK recv_callback(DWORD, DWORD, LPWSAOVERLAPPED, DWORD);
+void CALLBACK send_callback(DWORD, DWORD, LPWSAOVERLAPPED, DWORD);
+
 int main()
 {
 	std::wcout.imbue(std::locale("korean"));
@@ -69,7 +82,7 @@ int main()
 	WSAData WSAData;
 	WSAStartup(MAKEWORD(2, 2), &WSAData);
 
-	SOCKET s_socket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, 0, 0, 0);
+	SOCKET s_socket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, 0, 0, WSA_FLAG_OVERLAPPED);
 
 	if (s_socket <= 0)
 	{
@@ -90,58 +103,63 @@ int main()
 	listen(s_socket, SOMAXCONN);
 
 	INT addr_size = sizeof(SOCKADDR_IN);
-	SOCKET c_socket = WSAAccept(
+
+	c_socket = WSAAccept(
 		s_socket,
 		reinterpret_cast<sockaddr*>(&addr),
 		&addr_size,
 		NULL,
 		NULL);
 
+	send_callback(0, 0, 0, 0);
+
 	while (true)
 	{
-		char recv_buffer[1024];
-		WSABUF recv_wsabuf[1];
-		recv_wsabuf[0].len = sizeof(recv_buffer);
-		recv_wsabuf[0].buf = recv_buffer;
-
-		DWORD recv_bytes;
-		DWORD recv_flag = 0;
-
-		auto ret = WSARecv(c_socket, recv_wsabuf, 1, &recv_bytes, &recv_flag, NULL, NULL);
-
-		if (SOCKET_ERROR == ret)
-		{
-			std::cout << "Error at WSARecv";
-
-			auto s_err = WSAGetLastError();
-
-			//std::cout << s_err << std::endl;
-			print_error_message(s_err);
-			exit(-1);
-		}
-
-		recv_buffer[recv_bytes] = 0;
-		std::cout << "From Client : " << recv_buffer << std::endl;
-
-		std::string command(recv_buffer);
-		update_position(command);
-		std::cout << "Updated Position ( x : " << player_x << ", y : " << player_y << " ) " << std::endl;
-
-
-		char buffer[1024];
-		std::sprintf(buffer, "%d %d", player_x, player_y);
-
-		WSABUF wsabuf[1];
-		wsabuf[0].buf = buffer;
-		wsabuf[0].len = static_cast<ULONG>(std::strlen(buffer));
-
-		DWORD size_sent;
-
-		WSASend(c_socket, wsabuf, 1, &size_sent, 0, NULL, NULL);
+		SleepEx(0, TRUE);
 	}
 
 	closesocket(c_socket);
 	closesocket(s_socket);
 	WSACleanup();
-	return 0;
+}
+
+void CALLBACK send_callback(DWORD err, DWORD num_bytes, LPWSAOVERLAPPED p_over, DWORD flag)
+{
+	recv_wsabuf[0].len = sizeof(recv_buffer);
+	recv_wsabuf[0].buf = recv_buffer;
+
+	DWORD recv_flag = 0;
+
+	ZeroMemory(&recv_over, sizeof(recv_over));
+
+	auto ret = WSARecv(c_socket, recv_wsabuf, 1, NULL, &recv_flag, &recv_over, recv_callback);
+
+	if (0 != ret)
+	{
+		auto err_no = WSAGetLastError();
+		if (WSA_IO_PENDING != err_no)
+		{
+			print_error_message(err_no);
+			exit(-1);
+		}
+	}
+}
+
+void CALLBACK recv_callback(DWORD err, DWORD num_bytes, LPWSAOVERLAPPED p_over, DWORD flag)
+{
+	recv_buffer[num_bytes] = 0;
+	std::cout << "From Client : " << recv_buffer << std::endl;
+
+	std::string command(recv_buffer);
+	update_position(command);
+
+	std::sprintf(send_buffer, "%d %d", player_x, player_y);
+
+	send_wsabuf[0].buf = send_buffer;
+	send_wsabuf[0].len = static_cast<ULONG>(std::strlen(send_buffer));
+
+	ZeroMemory(&send_over, sizeof(send_over));
+	DWORD size_sent = 0;
+
+	WSASend(c_socket, send_wsabuf, 1, &size_sent, 0, &send_over, send_callback);
 }
